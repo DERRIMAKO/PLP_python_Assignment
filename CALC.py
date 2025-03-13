93b5022dd6856ac47f4282b0226fd0822b4c8da6
DERRICK MAKORI <119781154+DERRIMAKO@users.noreply.github.com>
2025-03-13 10:26:25 +0300

    Create CALC.py

---
+++ CALC.py
@@ -0,0 +1,26 @@
+def calculator():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    op = input("Enter operation (+, -, *, /): ")
+    
+    if op == '+':
+        result = num1 + num2
+    elif op == '-':
+        result = num1 - num2
+    elif op == '*':
+        result = num1 * num2
+    elif op == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            print("Cannot divide by zero.")
+            return
+    else:
+        print("Invalid operation.")
+        return
+    
+    print(num1, op, num2, "=", result)
+
+
+
+calculator()
